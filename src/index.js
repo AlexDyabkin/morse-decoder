5e67730bf46437fbc8806bd5bf20a618c167aa3d
AlexDyabkin <alexendredyabkin@gmail.com>
2020-03-22 14:22:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const PATTERN = /........../g;
+    const numberTen = /10/g;
+    const numberEleven = /11/g;
+    const numberZero = /00/g;
+    const space = /\*........./g;
+ 
+    let arr = expr.match(PATTERN);
+    
+    for(let i = 0; i < arr.length; i++) {
 
+       /*if(arr[i].includes('**********')) {
+            arr[i] = arr[i].replace(space, " ");
+        }*/
+
+        while(arr[i].includes('10')){
+            arr[i] = arr[i].replace(numberTen, ".");
+        }
+        while(arr[i].includes('11')){
+            arr[i] = arr[i].replace(numberEleven, "-");
+        }
+        while(arr[i].includes('00')){
+            arr[i] = arr[i].replace(numberZero, "");
+        }
+
+        
+        arr[i] = (MORSE_TABLE.hasOwnProperty(arr[i])) ? MORSE_TABLE[arr[i]] : " ";
+    }
+
+    return arr.join("");
+
+}
 module.exports = {
     decode
 }
